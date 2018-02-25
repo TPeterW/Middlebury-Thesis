d8bb7f325d5cd70f349d134eb0aee41366fab6f1
Tao Wang <taow@middlebury.edu>
2018-02-25 17:17:55 -0500

    Useless work

--- probes/consolidate.py
+++ probes/consolidate.py
@@ -2,10 +2,37 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from itertools import groupby, count
 
 def main():
-    pass
+	if len(sys.argv) < 2:
+		print('Usage: ./consolidate.py probes.list')
+		exit(1)
+
+	ids = []
+	with open(sys.argv[1]) as probefile:
+		for row in probefile:
+			ids.append(row.split()[0])	
+	
+	res = ','.join(as_range(g) for _, g in groupby(ids, key=lambda n, c=count(): int(n)-next(c)))
+
+	predicate = '/'
+	for r in res.split(','):
+		if '-' in r:
+			predicate += '(id >= '	+ r.split('-')[0] + ' && id <= ' + r.split('-')[1] + ') || '
+		else:
+			predicate += '(id == ' + r + ') || '
+	predicate = predicate[:-4] + '/'
+ 
+	print(predicate)
+
+def as_range(iterable): 
+    l = list(iterable)
+    if len(l) > 1:
+        return '{0}-{1}'.format(l[0], l[-1])
+    else:
+        return '{0}'.format(l[0])
 
 if __name__ == '__main__':
-    main()
+	main()
 
--- usb/all_usb_fbt.d
+++ usb/all_usb_fbt.d
@@ -4,14 +4,10 @@ dtrace:::BEGIN
 }
 
 fbt:kernel::entry
-/ (id >= 11321 && id <= 11355) || (id >= 11421 && id <= 11494) || (id >= 11526 && id <= 11530) || (id >= 11556 && id <= 11563) || (id >= 11581 && id <= 11604) /
-{
-	printf("%s\t %s\t %d\t %d\n", probefunc, execname, pid, ppid);
-}
-
-fbt:::return
+/strstr(probefunc, "usb") != 0/
 {
 	
+	printf("%s\t %s\t %d\t %d\n", probefunc, execname, pid, ppid);
 }
 
 dtrace:::END
