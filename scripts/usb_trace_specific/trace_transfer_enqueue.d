67aa52430640a7385fbbadc8b2fe82c790622147
Tao Wang <taow@middlebury.edu>
2018-03-05 12:17:12 -0500

    More

---
+++ scripts/usb_trace_specific/trace_transfer_enqueue.d
@@ -0,0 +1,23 @@
+#pragma D option flowindent
+
+fbt:kernel::entry
+/probefunc == "usbd_transfer_enqueue" && guard++ == 0/
+{
+	self->traceme = 1;
+	printf("fd: %d", arg0);
+}
+
+tpw:kernel::,
+fbt:kernel::
+/self->traceme/
+{
+	printf("tid: %d", tid);
+}
+
+fbt:::return
+/self->traceme/
+{
+	printf("tid: %d", tid);
+	self->traceme = 0;
+	exit(0);
+}
