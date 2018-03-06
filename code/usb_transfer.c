865dc06ed9d21bbd357545fb5b6283bfaa67f502
TPeterW <peterwangtao0@hotmail.com>
2018-03-05 22:36:04 -0500

    Make sure enqueue and dequeue probes fire

--- code/usb_transfer.c
+++ code/usb_transfer.c
@@ -2481,15 +2481,17 @@ usb_dma_delay_done_cb(struct usb_xfer *xfer)
 void
 usbd_transfer_dequeue(struct usb_xfer *xfer)
 {
+	SDT_PROBE2(tpw, kernel, usb_transfer_dequeue, entry, pq, xfer);
+
 	struct usb_xfer_queue *pq;
 
 	pq = xfer->wait_queue;
 	if (pq) {
-		SDT_PROBE2(tpw, kernel, usb_transfer_dequeue, entry, pq, xfer);
 		TAILQ_REMOVE(&pq->head, xfer, wait_entry);
 		xfer->wait_queue = NULL;
-		SDT_PROBE2(tpw, kernel, usb_transfer_dequeue, return, pq, xfer);
 	}
+
+	SDT_PROBE2(tpw, kernel, usb_transfer_dequeue, return, pq, xfer);
 }
 
 /*------------------------------------------------------------------------*
@@ -2503,16 +2505,18 @@ usbd_transfer_dequeue(struct usb_xfer *xfer)
 void
 usbd_transfer_enqueue(struct usb_xfer_queue *pq, struct usb_xfer *xfer)
 {
+	SDT_PROBE2(tpw, kernel, usb_transfer_enqueue, entry, pq, xfer);
+
 	/*
 	 * Insert the USB transfer into the queue, if it is not
 	 * already on a USB transfer queue:
 	 */
 	if (xfer->wait_queue == NULL) {
-		SDT_PROBE2(tpw, kernel, usb_transfer_enqueue, entry, pq, xfer);
 		xfer->wait_queue = pq;
 		TAILQ_INSERT_TAIL(&pq->head, xfer, wait_entry);
-		SDT_PROBE2(tpw, kernel, usb_transfer_enqueue, return, pq, xfer);
 	}
+	
+	SDT_PROBE2(tpw, kernel, usb_transfer_enqueue, return, pq, xfer);
 }
 
 /*------------------------------------------------------------------------*
