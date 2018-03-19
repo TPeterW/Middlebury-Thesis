57af724bc13a22d040283aad4cc537585a9944d7
Tao Wang <taow@middlebury.edu>
2018-03-19 00:09:34 -0400

    Now tracks all the tpw probes with queue addresses for enqueue and dequeue

--- scripts/tpw_probes/all_tpw_queue.d
+++ scripts/tpw_probes/all_tpw_queue.d
@@ -3,9 +3,17 @@ dtrace:::BEGIN
 	trace("Begin listening...\n");
 }
 
+tpw:kernel:usb_transfer_usbd_transfer_enqueue:entry,
+tpw:kernel:usb_transfer_usbd_transfer_dequeue:entry,
+tpw:kernel:usb_transfer_usbd_transfer_enqueue:return,
+tpw:kernel:usb_transfer_usbd_transfer_dequeue:return
+{
+	printf("%s\t%d\t%s\t%p\t%p\t", execname, pid, probefunc, arg0, arg1);
+}
+
 tpw:kernel::
 {
-	printf("%s\t%d\t%d\t%s\t%p\t%p\t", execname, pid, ppid, probefunc, arg0, arg1);
+	printf("%s\t%d\t%s\t", execname, pid, probefunc);
 }
 
 dtrace:::END
