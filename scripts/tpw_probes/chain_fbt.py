11c97d28b53c0823f02a242adc7678f9c5c097be
TPeterW <peterwangtao0@hotmail.com>
2018-03-19 12:28:02 -0400

    Polish up the script

--- scripts/tpw_probes/chain_fbt.py
+++ scripts/tpw_probes/chain_fbt.py
@@ -17,30 +17,42 @@ def main():
 	
 	lines = lines[2:-1]
 
+	# current_func_on_threads = {}
 	current_func = []
+	current_process = -1
 	indent_level = 0
 	for entry in lines:
 		splitted = entry.split()
 
 		funcname = splitted[5]
-		pid = splitted[4]
+		pid = int(splitted[4])
 		provider = splitted[3]
 		functype = splitted[2].split(':')[1]
 
 		if functype == 'entry':
+			if current_process != pid:
+				for i in range(indent_level):
+					print('\t', end='')
+				print('====================Switched to process %s====================' % (pid))
+				current_process = pid
 			for i in range(indent_level):
 				print('\t', end='')
-			print('-> %s' % funcname)
+			print('-> %s\t%s' % (funcname, pid))
 			indent_level += 1
 			current_func.append(funcname)
 		else:
 			if len(current_func) <= 0:
 				continue
 			if current_func[-1] == funcname:
+				if current_process != pid:
+					for i in range(indent_level):
+						print('\t', end='')
+					print('====================Switched to process %s====================' % (pid))
+					current_process = pid
 				indent_level -= 1
 				for i in range(indent_level):
 					print('\t', end='')
-				print('<- %s' % funcname)
+				print('<- %s\t%s' % (funcname, pid))
 				current_func = current_func[:-1]
 
 
